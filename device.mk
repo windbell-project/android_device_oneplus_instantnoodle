d552277f59203dd407697e68d4b7e4595ca14d5e
kidachiP <1376106928@qq.com>
2021-08-06 23:48:11 +0800

    instantnoodle: Nortical-ify

--- device.mk
+++ device.mk
@@ -24,8 +24,7 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
